400fd42f6bc9000527caec3dbf0775e575590133
vivek3894 <viveknagaraju@yahoo.in>
2018-07-16 13:49:09 +0530

    storing the data to local storage

--- todo-functions.js
+++ todo-functions.js
@@ -12,7 +12,7 @@ const savedTodos = function() {
 
 // get filtered todos
 
-const liveFilteredTodos =  function(todos, filters) {
+const liveFilteredTodos = function(todos, filters) {
 	let filteredTodos = todos.filter((todo) => {
 		return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
 	})
@@ -26,43 +26,43 @@ const liveFilteredTodos =  function(todos, filters) {
 	})
 
 
-const saveToLocalStorage = function(noteId){
-	localStorage.setItem('todos', JSON.stringify(todos));
-}	
+	const saveToLocalStorage = function(noteId) {
+		localStorage.setItem('todos', JSON.stringify(todos));
+	}
 
-const removeTodos = function(todoId) {
-   let index = todos.findIndex(function(todo){
-   	return todo.id === todoId;
-   });
+	const removeTodos = function(todoId) {
+		let index = todos.findIndex(function(todo) {
+			return todo.id === todoId;
+		});
 
-   if(index != -1){
-   	  todos.splice(index,1);
-   	 saveToLocalStorage();
-   }
-}
+		if (index != -1) {
+			todos.splice(index, 1);
+			saveToLocalStorage();
+		}
+	}
 
 
 
-// adding delete button to all the todos
+	// adding delete button to all the todos
 	document.querySelector('#all-todos').innerHTML = '';
-    const container = document.createElement('div');
-    document.querySelector('#all-todos').appendChild(container);
-    const deleteButton = document.createElement('button');
+	const container = document.createElement('div');
+	document.querySelector('#all-todos').appendChild(container);
+	const deleteButton = document.createElement('button');
 	filteredTodos.forEach((todo) => {
 		const deleteButton = document.createElement('button');
 		deleteButton.addEventListener('click', function() {
-            removeTodos(todo.id);
-            liveFilteredTodos(todos , filters);
+			removeTodos(todo.id);
+			liveFilteredTodos(todos, filters);
 		});
 		const check = document.createElement('input');
 		check.setAttribute('type', 'checkbox');
-		if(todo.completed){
+		if (todo.completed) {
 			check.checked = true;
 		}
 		check.addEventListener('change', function() {
-          todo.completed = !todo.completed;
-          saveToLocalStorage(todo.id)
-          liveFilteredTodos(todos, filters);
+			todo.completed = !todo.completed;
+			saveToLocalStorage(todo.id)
+			liveFilteredTodos(todos, filters);
 		})
 		deleteButton.textContent = 'delete'
 		const ele = document.createElement('span');
